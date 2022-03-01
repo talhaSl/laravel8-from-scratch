9d5a4da7d0ffd007c9f448d28bc44b21e4fedbc5
Umar <waqasch443@gmail.com>
2022-03-01 11:41:47 +0500

    ep n issue in ymal formatter

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,4 +1,33 @@
 <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=
+    , initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>My Blog</title>
+
+
+    <link rel="stylesheet" href="/app.css">
+    {{-- <script src ="app.js"></script> --}}
+</head>
+<body>
+    <article>
+        <?= $post; ?>
+
+        {{-- <h1>My First Post</h1>
+        <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum
+        </p> --}}
+    </article>
+
+
+    <a href="/">Go Back</a>
+</body>
+</html>
+
+
+{{-- <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
@@ -129,4 +158,4 @@
             </div>
         </div>
     </body>
-</html>
+</html> --}}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
+use Spatie\YamlFrontMatter\YamlFrontMatter;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +16,19 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+
+    $doc= YamlFrontMatter::parseFile(
+        resource_path('posts/3rdPost.html')
+    );
+
+    ddd($doc);
+    // $posts = Post::all();
+    // // ddd ($posts);
+    // return view('posts',['posts'=>$posts]);
 });
+
+Route::get('post/{post}', function ($slug) {
+
+    $post = Post::find($slug);
+    return view('post', ['post' => $post]);
+})->where('post', '[A-z_\-]+');
